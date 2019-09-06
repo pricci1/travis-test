649dad746a5a42c826a184295c4d405e5b7eef30
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-06 03:44:37 +0000

    Deploy pricci1/travis-test to github.com/pricci1/travis-test.git:gh-pages

---
+++ precache-manifest.0f3e67849ae9fd59b6a3d31aea1d9b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1684510febce19869cd765c0a79889db",
+    "url": "/travis-test/index.html"
+  },
+  {
+    "revision": "3119e453d0aff281a241",
+    "url": "/travis-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "74acd7de0e35b140992d",
+    "url": "/travis-test/static/js/2.c9d5257b.chunk.js"
+  },
+  {
+    "revision": "3119e453d0aff281a241",
+    "url": "/travis-test/static/js/main.f25ce049.chunk.js"
+  },
+  {
+    "revision": "625fdea2ddc99b95fd23",
+    "url": "/travis-test/static/js/runtime~main.75cb53e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
